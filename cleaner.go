bc74c9eaba51211da393583266cca090d2a7a94d
Jeremy Deininger <jeremydeininger@gmail.com>
2017-05-08 16:03:22 -0700

    adding INFLUXDB_INTERVAL for running periodically

--- cleaner.go
+++ cleaner.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/influxdata/influxdb/client/v2"
 	"github.com/spf13/viper"
@@ -15,17 +18,14 @@ import (
 // INFLUXDB_URL=http://myinflux:8086
 // INFLUXDB_DRYRUN=true // for dry run
 
-func check_if_set(name string) {
+func checkIfSet(name string) {
 	if !viper.IsSet(name) {
 		fmt.Printf("You must set the environment variable $INFLUXDB_%s, exiting..\n", name)
 		os.Exit(1)
 	}
 }
 
-func main() {
-	viper.SetEnvPrefix("influxdb")
-	viper.AutomaticEnv()
-	check_if_set("url")
+func clean() {
 	// Create a new HTTPClient
 	c, err := client.NewHTTPClient(client.HTTPConfig{
 		Addr: viper.GetString("url"),
@@ -56,5 +56,30 @@ func main() {
 				}
 			}
 		}
+	}
+}
+
+func main() {
+	viper.SetEnvPrefix("influxdb")
+	viper.AutomaticEnv()
+	checkIfSet("url")
+	// If interval is set, re-run every interval hours.
+	if viper.IsSet("interval") {
+		fmt.Printf("Cleaning subscriptions every %d seconds\n", viper.GetInt("interval"))
+		ticker := time.NewTicker(time.Second * viper.GetDuration("interval"))
+		go func() {
+			for t := range ticker.C {
+				fmt.Println("Starting cleaning run ", t)
+				clean()
+			}
+		}()
+
+		// wait for exit
+		exitSignal := make(chan os.Signal)
+		signal.Notify(exitSignal, syscall.SIGINT, syscall.SIGTERM)
+		<-exitSignal
+
+	} else {
+		clean()
 	}
 }
